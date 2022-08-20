2026ba2015e70991d95d1f97bca879ca80903f24
Adithya Nair <adityasnair2000@gmail.com>
2022-08-20 12:25:11 +0530

    added changes

--- aniflix/src/routes/landing/landing.component.jsx
+++ aniflix/src/routes/landing/landing.component.jsx
@@ -1,6 +1,7 @@
 import './landing.styles.css';
 import { Button } from '../../components/custom-button/custom-button.component';
 import { useNavigate } from 'react-router-dom';
+import {motion} from 'framer-motion';
 export const LandingPage = () => {
     const navigate = useNavigate();
     const goToAuthentication = () => {
@@ -13,12 +14,16 @@ export const LandingPage = () => {
             backgroundSize: "cover",
             backgroundRepeat: "no-repeat"
         }}>
-        <div className='landing-page-contents'>
+        <motion.div 
+        className='landing-page-contents'
+        initial={{y:"100%",opacity:0.2,scale:0.5}}
+        animate={{y:0,opacity:1,scale:1}}
+        transition={{duration:0.5,ease:"easeIn"}}>
             <h1>Welcome to Aniflix</h1>
             <h2>Unlimited films, TV Programmes and more.</h2>
             <p>Watch anywhere. Cancel at any time.</p>
             <Button onClick={goToAuthentication} buttonType="getStarted">get started</Button>
-        </div>
+        </motion.div>
         <div className='overlay'></div>
         </div>
     )
